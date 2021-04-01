071fba7868801ccee8f33d64530116e21b4c4e63
JuneCheng <755038283@qq.com>
2021-04-01 15:19:36 +0800

    demo修改

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 
-import 'package:flutter/services.dart';
 import 'package:ccms_des/ccms_des.dart';
 
 void main() {
@@ -14,34 +13,23 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-  final _string = "JuneCheng7";
-  final _key = "zjhcsoft123!@#";
+  final _string = "testMessage";
+  final _key = "12345678";
+
+  var _encrytString;
+  var _decrytString;
 
   @override
   void initState() {
     super.initState();
-    initPlatformState();
+    example();
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await CcmsDes.encryptToHex(_string, _key);
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
+  Future<void> example() async {
+    _encrytString = await CcmsDes.encryptToHex(_string, _key);
+    _decrytString = await CcmsDes.decryptFromHex(_encrytString, _key);
 
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
+    setState(() {});
   }
 
   @override
@@ -49,10 +37,20 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('des example app'),
         ),
         body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Padding(padding: EdgeInsets.all(10)),
+              Text('_string : $_string'),
+              Text('_key : $_key'),
+              Padding(padding: EdgeInsets.all(10)),
+              Text('encrytString : $_encrytString'),
+              Text('decrytString : $_decrytString'),
+            ],
+          ),
         ),
       ),
     );
